2158c30124b05d7045ecf401fd557859aff87109
Satoshi Yoshikawa <yoshikawa@fairydevices.jp>
2023-05-25 08:06:18 +0900

    change order of functions

--- src/bin/plot_csv.rs
+++ src/bin/plot_csv.rs
@@ -8,40 +8,27 @@ const FILENAME: &str = "./data/2020413_utf8.csv";
 const FONT_STYLE: (&str, f64) = ("Hiragino Maru Gothic Pro", 20.0);
 const FIG_SIZE: (u32, u32) = (640, 300);
 
-/// `AnyValue`の日時情報を`DateTime<Utc>`に変換する関数
-fn value_to_utc(value: AnyValue) -> anyhow::Result<DateTime<Utc>> {
-    let x = NaiveDateTime::from(&value);
-    let d = x
-        .and_local_timezone(Utc)
-        .single()
-        .context("couldn't convert to UTC timestamp.")?;
-    Ok(d)
-}
+fn main() -> anyhow::Result<()> {
+    let lf = LazyCsvReader::new(FILENAME)
+        .with_try_parse_dates(true)
+        .has_header(true)
+        .with_ignore_errors(true)
+        .with_encoding(CsvEncoding::Utf8)
+        .finish()?;
 
-/// `DataFrame`から日時を1件抽出し`DateTime<Utc>`にして返す関数
-fn df_to_utc(df: &DataFrame) -> anyhow::Result<DateTime<Utc>> {
-    value_to_utc(df.column("")?.get(0)?)
-}
+    let plot_list = [
+        ("気化器液面レベル_PV", "level_pv"),
+        ("気化器液面レベル_SV", "level_sv"),
+        ("気化器液面レベル_MV", "level_mv"),
+        ("コンプレッサー出口温度_PV", "comp_pv"),
+        ("コンプレッサー出口温度_SV", "comp_sv"),
+    ];
 
-/// `LazyFrame`の日時列の一番古い・新しい時刻で`Range`を作る
-fn get_date_range(
-    lf: &LazyFrame,
-) -> anyhow::Result<Range<DateTime<Utc>>> {
-    let from = lf.clone().select([col("")]).min().collect()?;
-    let to = lf.clone().select([col("")]).max().collect()?;
+    for (col_name, file_name) in plot_list {
+        plot_figs(&lf, col_name, file_name)?;
+    }
 
-    Ok(Range {
-        start: df_to_utc(&from)?,
-        end: df_to_utc(&to)?,
-    })
-}
-
-/// `f64`の`Series`の上限＆下限をRangeにして返す
-fn get_value_range(series: &Series) -> anyhow::Result<Range<f64>> {
-    Ok(Range {
-        start: series.min().context("couldn't find a min value")?,
-        end: series.max().context("couldn't find a max value")?,
-    })
+    Ok(())
 }
 
 /// `LazyFrame`から指定した列のグラフを出力する
@@ -92,25 +79,38 @@ fn plot_figs(
     Ok(())
 }
 
-fn main() -> anyhow::Result<()> {
-    let lf = LazyCsvReader::new(FILENAME)
-        .with_try_parse_dates(true)
-        .has_header(true)
-        .with_ignore_errors(true)
-        .with_encoding(CsvEncoding::Utf8)
-        .finish()?;
+/// `AnyValue`の日時情報を`DateTime<Utc>`に変換する関数
+fn value_to_utc(value: AnyValue) -> anyhow::Result<DateTime<Utc>> {
+    let x = NaiveDateTime::from(&value);
+    let d = x
+        .and_local_timezone(Utc)
+        .single()
+        .context("couldn't convert to UTC timestamp.")?;
+    Ok(d)
+}
 
-    let plot_list = [
-        ("気化器液面レベル_PV", "level_pv"),
-        ("気化器液面レベル_SV", "level_sv"),
-        ("気化器液面レベル_MV", "level_mv"),
-        ("コンプレッサー出口温度_PV", "comp_pv"),
-        ("コンプレッサー出口温度_SV", "comp_sv"),
-    ];
+/// `DataFrame`から日時を1件抽出し`DateTime<Utc>`にして返す関数
+fn df_to_utc(df: &DataFrame) -> anyhow::Result<DateTime<Utc>> {
+    value_to_utc(df.column("")?.get(0)?)
+}
 
-    for (col_name, file_name) in plot_list {
-        plot_figs(&lf, col_name, file_name)?;
-    }
+/// `LazyFrame`の日時列の一番古い・新しい時刻で`Range`を作る
+fn get_date_range(
+    lf: &LazyFrame,
+) -> anyhow::Result<Range<DateTime<Utc>>> {
+    let from = lf.clone().select([col("")]).min().collect()?;
+    let to = lf.clone().select([col("")]).max().collect()?;
 
-    Ok(())
+    Ok(Range {
+        start: df_to_utc(&from)?,
+        end: df_to_utc(&to)?,
+    })
+}
+
+/// `f64`の`Series`の上限＆下限をRangeにして返す
+fn get_value_range(series: &Series) -> anyhow::Result<Range<f64>> {
+    Ok(Range {
+        start: series.min().context("couldn't find a min value")?,
+        end: series.max().context("couldn't find a max value")?,
+    })
 }
